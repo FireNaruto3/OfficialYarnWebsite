1e72c1b94a11b059c588fd465d21480811c7195d
Jonathan <youremail@yourdomain.com>
2024-06-03 22:37:58 -0700

    fixed form, need to work on how to get post and get requests to work https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_and_retrieving_form_data, use this website

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def homePage():
     return render_template("home.html")
 
+@app.route("/questions.html", methods = ["GET", "POST"])
+def questionPage():
+    return render_template("questions.html")
 
 if(__name__ == '__main__'):
     app.run(debug = True)
